91e11834fa8b2b2f734a4e38b96dc5c225ac82c0
subakpup <129526142+subakpup@users.noreply.github.com>
2026-02-16 11:22:23 +0900

    [Bronze I] Title: 단어 공부, Time: 160 ms, Memory: 20528 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		int[] alpha = new int[26];
+		
+		for (int i = 0; i < s.length(); i++) {
+			if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
+				++alpha[s.charAt(i) - 'a'];
+			} else {
+				++alpha[s.charAt(i) - 'A'];
+			}
+		}
+		
+		int max = -1;
+		char c = '?';
+		for (int i = 0; i < 26; i++) {
+			if (alpha[i] > max) {
+				max = alpha[i];
+				c = (char) (i + 'A');
+			} else if (alpha[i] == max) {
+				c = '?';
+			}
+		}
+		
+		System.out.println(c);
+	}
+
+}
