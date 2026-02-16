b219f976fd0cbd87b651a68efd95b00e3594011b
subakpup <129526142+subakpup@users.noreply.github.com>
2026-02-16 11:55:59 +0900

    [Silver II] Title: N과 M (10), Time: 64 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int n, m;
+	static int[] num, arr;
+	static boolean[] visited;
+	
+	private static void dfs(int depth, int start) {
+		if (depth == m) {
+			for (int a : arr) sb.append(a).append(' ');
+			sb.append('\n');
+			return;
+		}
+		
+		int prev = 0;
+		for (int i = start; i < n; i++) {
+			if (!visited[i] && prev != num[i]) {
+				visited[i] = true;
+				prev = num[i];
+				arr[depth] = num[i]; 
+				dfs(depth + 1, i + 1);
+				visited[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		num = new int[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		
+		visited = new boolean[n];
+		arr = new int[m];
+		
+		dfs(0, 0);
+		System.out.println(sb.toString());
+	}
+
+}
