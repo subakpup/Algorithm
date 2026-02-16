8a38b5e43a4673b59e96aa3f1a91fe3b6d9d9776
subakpup <129526142+subakpup@users.noreply.github.com>
2026-02-16 11:49:04 +0900

    [Bronze V] Title: 개수 세기, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int v = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == v) cnt++;
+		}
+		System.out.println(cnt);
+		
+	}
+}
