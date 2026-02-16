79b948f5087fbe98b13858e0a5aad338c5df61b3
subakpup <129526142+subakpup@users.noreply.github.com>
2026-02-16 12:41:43 +0900

    [Bronze III] Title: 행복, Time: 76 ms, Memory: 11896 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15969. 행복/행복.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int max = -1; int min = 1001;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        System.out.println(max - min);
+    }
+}
