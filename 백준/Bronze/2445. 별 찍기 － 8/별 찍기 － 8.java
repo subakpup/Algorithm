787cdcd8023f30eb6c9b0f6b9380f19b571468a2
subakpup <129526142+subakpup@users.noreply.github.com>
2026-02-24 09:16:51 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 68 ms, Memory: 11808 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -8,14 +8,14 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) sb.append('*');
-            for (int j = (n - i) * 2; j > 0; j--) sb.append(' ');
+            for (int j = 0; j < (n - i) * 2; j++) sb.append(' ');
             for (int j = 0; j < i; j++) sb.append('*');
             sb.append('\n');
         }
 
         for (int i = n - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) sb.append('*');
-            for (int j = (n - i) * 2; j > 0; j--) sb.append(' ');
+            for (int j = 0; j < (n - i) * 2; j++) sb.append(' ');
             for (int j = 0; j < i; j++) sb.append('*');
             sb.append('\n');
         }
