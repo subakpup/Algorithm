9ab2fcc945c777f37d87bbaf7d018a1393237bed
subakpup <129526142+subakpup@users.noreply.github.com>
2026-02-24 09:14:53 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 68 ms, Memory: 11928 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) sb.append('*');
+            for (int j = (n - i) * 2; j > 0; j--) sb.append(' ');
+            for (int j = 0; j < i; j++) sb.append('*');
+            sb.append('\n');
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) sb.append('*');
+            for (int j = (n - i) * 2; j > 0; j--) sb.append(' ');
+            for (int j = 0; j < i; j++) sb.append('*');
+            sb.append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
