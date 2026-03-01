0aae2097ea429bec5bad9e62b83a56c863001962
subakpup <129526142+subakpup@users.noreply.github.com>
2026-03-01 15:14:15 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 68 ms, Memory: 11652 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) sb.append(' ');
+            for (int j = 0; j < i; j++) sb.append('*').append(' ');
+            sb.append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
