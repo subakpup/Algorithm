ad744a43c9ccfab4fd274632d6c3204c69dcc212
subakpup <129526142+subakpup@users.noreply.github.com>
2026-03-09 16:48:05 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 11792 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    private static String solve(String s) {
+        int cnt = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') ++cnt;
+            else if (c == ')') {
+                if (cnt <= 0) return "NO";
+                --cnt;
+            }
+        }
+
+        if (cnt == 0) return "YES";
+        return "NO";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            sb.append(solve(s)).append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
