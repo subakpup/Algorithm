ac129597ca00556638d8344de4cfc29bfea71fe1
subakpup <129526142+subakpup@users.noreply.github.com>
2026-03-21 12:13:51 +0900

    [Bronze IV] Title: ZOAC 6, Time: 88 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/30045. ZOAC 6/ZOAC 6.java
+++ 백준/Bronze/30045. ZOAC 6/ZOAC 6.java
@@ -1,9 +1,6 @@
-import java.util.*;
 import java.io.*;
 
 class Main {
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
