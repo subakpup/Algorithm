ef54bb950275bf4515ae900dd9d3a07c1ec9c3e3
subakpup <129526142+subakpup@users.noreply.github.com>
2026-03-23 21:18:46 +0900

    [Bronze IV] Title: 인간은 무엇인가, Time: 68 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/32651. 인간은 무엇인가/인간은 무엇인가.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n <= 100000 && n % 2024 == 0 ? "Yes" : "No");
+    }
+}
