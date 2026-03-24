11eca8b470caf83a7c81ef25ae64f3fb5e1921c9
subakpup <129526142+subakpup@users.noreply.github.com>
2026-03-24 20:49:38 +0900

    [Silver III] Title: 프린터 큐, Time: 96 ms, Memory: 12412 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            Deque<Integer> deque = new ArrayDeque<>();
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) deque.add(Integer.parseInt(st.nextToken()));
+
+            int cnt = 0;
+
+            while (!deque.isEmpty()) {
+                int max = Collections.max(deque);
+                int first = deque.peekFirst();
+                --m;
+
+                if (max == first) {
+                    ++cnt;
+                    deque.pollFirst();
+                    
+                    if (m < 0) {
+                        sb.append(cnt).append('\n');
+                        break;
+                    }
+                } else {
+                    deque.addLast(deque.pollFirst());
+                    if (m < 0) m = deque.size() - 1;
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
