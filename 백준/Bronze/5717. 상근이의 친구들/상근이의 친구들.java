f78774ed45c44d8b86a1c42f16644b2af787fb9c
subakpup <129526142+subakpup@users.noreply.github.com>
2026-04-03 17:40:31 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 68 ms, Memory: 11724 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) break;
+
+            sb.append(a + b).append('\n');
+        }
+
+        System.out.println(sb.toString());
+    }
+}
