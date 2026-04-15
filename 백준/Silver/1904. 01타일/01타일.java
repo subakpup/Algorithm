5c3708c6446b6865ed24dcbd00373033d79d7400
subakpup <129526142+subakpup@users.noreply.github.com>
2026-04-15 10:30:00 +0900

    [Silver III] Title: 01타일, Time: 224 ms, Memory: 58072 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -2,22 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int[] dp;
+
+    private static int solve(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        if (dp[n] != -1) return dp[n];
+
+        dp[n] = (solve(n - 1) + solve(n - 2)) % 15746;
+        return dp[n];
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[n+1];
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
 
-        dp[1] = 1;
-
-        if (n > 1) {
-            dp[2] = 2;
-            for (int i = 3; i <= n; i++) {
-                dp[i] = (dp[i-1] + dp[i-2]) % 15746;
-            }
-        }
-        
-        System.out.println(dp[n]);
+        System.out.println(solve(n));
     }
 }
