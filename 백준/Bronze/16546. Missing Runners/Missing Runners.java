89a9a635d615c3c20f44ccbe7b5fdb1c7e678e59
subakpup <129526142+subakpup@users.noreply.github.com>
2026-04-18 11:41:10 +0900

    [Bronze III] Title: Missing Runners, Time: 284 ms, Memory: 23768 KB -BaekjoonHub

--- 백준/Bronze/16546. Missing Runners/Missing Runners.java
+++ 백준/Bronze/16546. Missing Runners/Missing Runners.java
@@ -1,12 +1,21 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+
+        System.out.println(n);
+    }
+}
