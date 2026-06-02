d1cfe47697d96649c301982777b3f170e14f88e0
subakpup <129526142+subakpup@users.noreply.github.com>
2026-06-02 20:44:01 +0900

    [level 1] Title: 폰켓몬, Time: 6.41 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        
+        return Math.min(nums.length / 2, set.size());
+    }
+}
