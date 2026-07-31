8ae2902d339ba55b3713f4cea9f1fcd6c34fbdde
함지수 <129526142+subakpup@users.noreply.github.com>
2026-07-31 22:09:03 +0900

    [level 0] Title: 문자열 출력하기, Time: 80.92 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+    }
+}
