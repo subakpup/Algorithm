2b0adedb83f1292af51165d8a6cd68e55dbcfa36
함지수 <129526142+subakpup@users.noreply.github.com>
2026-09-01 16:19:32 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.60 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        while (true) {
+            answer.add(n);
+            
+            if (n == 1) break;
+            
+            if (n % 2 == 0) n /= 2;
+            else n = 3 * n + 1;
+        }
+        
+        return answer;
+    }
+}
