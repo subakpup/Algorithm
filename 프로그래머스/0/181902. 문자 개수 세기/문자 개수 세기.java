03577f8ea2b4744e61468d9d863cb56052eb0be7
함지수 <129526142+subakpup@users.noreply.github.com>
2026-09-17 13:06:45 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.06 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if (c <= 'Z') answer[c - 'A']++;
+            else answer[c - 'a' + 26]++;
+        }
+        
+        return answer;
+    }
+}
