7ab250aa785677e8facfcd47725fbf5481a8cd31
함지수 <129526142+subakpup@users.noreply.github.com>
2026-09-20 16:35:31 +0900

    [level 0] Title: 카운트 다운, Time: 0.06 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int start_num, int end_num) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = start_num; i >= end_num; i--) {
+            answer.add(i);
+        }
+        
+        return answer;
+    }
+}
